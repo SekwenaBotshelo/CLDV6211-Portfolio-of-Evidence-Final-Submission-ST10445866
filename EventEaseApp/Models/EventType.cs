5a39acc8b66c30dbc5cc2901586a2a3952aeb490
SekwenaBotshelo <st10445866@imconnect.edu.za>
2025-06-23 10:12:33 +0200

    First Commit for ST10445866

---
+++ EventEaseApp/Models/EventType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EventEaseApp.Models
+{
+    public class EventType
+    {
+        public int EventTypeID { get; set; }
+
+        [Required(ErrorMessage = "Please select an Event Type.")]
+        public string TypeName { get; set; }
+
+        // Navigation property to related events
+        public virtual ICollection<Event> Events { get; set; }
+    }
+}
